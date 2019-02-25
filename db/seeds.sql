9d604b492a4cf2a02f67a4235ab85990ac708cc2
jcasas5253 <skedabble@gmail.com>
2019-02-24 18:07:00 -0800

    app setup; config setup; view setup; db setup; controller setup; model setup;

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+USE `burgers_db`;
+
+
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Bacon', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Angus', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Veggie', false);
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var expshdl = require('express-handlebars');
+app.engine('handlebars', expshdl({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var router = require('./controllers/burgers_controller.js');
+app.use('/', router);
+
+
+
+app.listen(PORT, function() 
+{
+  console.log("App connected on " + PORT);
+});
+
+
+    
