7c9b935e8a504b4201c1b142b5a5b2236d2a1d64
jcasas5253 <skedabble@gmail.com>
2019-02-24 21:59:17 -0800

    added code for jaws_db

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,17 @@ var connection = mysql.createConnection({
 
 });
 
+if (proccess.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db'
+    });
+};
+
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
