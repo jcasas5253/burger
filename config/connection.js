9526b764abfce60f5dcfb6b26018b1d8a4e6be17
jcasas5253 <skedabble@gmail.com>
2019-02-26 19:21:45 -0800

    heroku

--- config/connection.js
+++ config/connection.js
@@ -20,9 +20,9 @@ if (proccess.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'hacktheplanet',
+        host: 'o677vxfi8ok6exrd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'ddl63npcqu4uv16e',
+        password: 'xe65pw7v34cih50o',
         database: 'todoagain_db'
     });
 };
